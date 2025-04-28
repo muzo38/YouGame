3041c72a7bc785832a0d9644f609724df9f92922
Šarūnas Gromavičius <sarasgrom@gmail.com>
2025-04-28 12:24:27 +0300

    Added main files via upload.

---
+++ ProjectUnknown/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome ladies and gentlemen. Today we are going to create a project unknown to us.");
+    }
+}
