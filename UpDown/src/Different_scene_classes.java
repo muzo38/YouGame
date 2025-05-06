6d0efabd9936f25744c7d57d6b39270c5ce98ceb
Mantas Lamauskas <lamauskas.mantas@gmail.com>
2025-05-07 00:22:08 +0300

    Added classes to work

---
+++ UpDown/src/Different_scene_classes.java
@@ -0,0 +1,7 @@
+
+//This class will be used for handling the story as text through multiple files
+//or possibly even a single large file containing the entire story (still to be decided).
+
+public class Different_scene_classes {
+    
+}
---
+++ UpDown/src/GameWindow.java
@@ -0,0 +1,6 @@
+
+//This class will be responsible for showing a window when the program is launched.
+
+public class GameWindow {
+    
+}
