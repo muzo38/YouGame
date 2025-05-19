dd72a3e35ca6ab9ea4a8de9555e683a18e3010c8
Šarūnas Gromavičius <sarasgrom@gmail.com>
2025-05-19 21:14:02 +0300

    Update SceneMoment.java

--- UpDown/src/SceneMoment.java
+++ UpDown/src/SceneMoment.java
@@ -1,7 +1,7 @@
 //mantas.lamauskas@mif.stud.vu.lt
 //šarūnas.gromavičius@mif.stud.vu.lt
 
-// A bloated data class that should have been a record instead.
+// A data class, maybe better a record?
 public class SceneMoment {
     public String storyText;
     public String continuationPrompt;
@@ -10,7 +10,7 @@ public class SceneMoment {
     public String backgroundImagePath; // optional (future)
     public String ambientMusicPath;    // optional (future)
 
-// Basic constructor for when you're too lazy to provide choices.
+// Basic constructor for when not providing choices.
     public SceneMoment(String storyText, String continuationPrompt) {
         this.storyText = storyText;
         this.continuationPrompt = continuationPrompt;
@@ -23,7 +23,7 @@ public class SceneMoment {
         this.finalChoices = finalChoices;
     }
 
-// Extended constructor that I added for future features we'll probably never implement.
+// Extended constructor added for future features.
     public SceneMoment(String storyText, String continuationPrompt, String finalQuestion, String[] finalChoices, String imagePath, String musicPath) {
         this(storyText, continuationPrompt, finalQuestion, finalChoices);
         this.backgroundImagePath = imagePath;
