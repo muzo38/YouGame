3f3dd8e3eb9f3c38680690d4e3fd2c569eb18b6e
Mantas Lamauskas <lamauskas.mantas@gmail.com>
2025-05-19 19:35:42 +0300

    Delete UpDown

--- UpDown/src/SceneListener.java
+++
@@ -1,8 +0,0 @@
-//mantas.lamauskas@mif.stud.vu.lt
-//šarūnas.gromavičius@mif.stud.vu.lt
-
-// An interface so absurdly simple it barely deserves to exist.
-public interface SceneListener {
-// Called when a scene is finally over, thank god.
-    void onSceneFinished();
-}
