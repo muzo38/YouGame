ceed50117be6493c8afc717e31796f109d320d2d
Šarūnas Gromavičius <sarasgrom@gmail.com>
2025-05-19 21:11:21 +0300

    Update SceneListener.java

--- UpDown/src/SceneListener.java
+++ UpDown/src/SceneListener.java
@@ -1,7 +1,6 @@
 //mantas.lamauskas@mif.stud.vu.lt
 //šarūnas.gromavičius@mif.stud.vu.lt
 
-// An interface so absurdly simple it barely deserves to exist.
 public interface SceneListener {
 // Called when a scene is finally over, thank god.
     void onSceneFinished();
