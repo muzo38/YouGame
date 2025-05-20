b820c0310d472ffb5f8b98fcf5c3baa95706206e
Šarūnas Gromavičius <sarasgrom@gmail.com>
2025-05-20 20:36:13 +0300

    Update Main.java

--- UpDown/src/Main.java
+++ UpDown/src/Main.java
@@ -2,7 +2,7 @@
 //šarūnas.gromavičius@mif.stud.vu.lt
 
 public class Main {
-// Main method that does the bare minimum needed to start this mess of an application.
+// starts this mess of an application
     public static void main(String[] args) {
         new GameWindow();
     }
