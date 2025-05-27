551d9a53442126129ec5a6a962431e8fdf047924
Mantas Lamauskas <lamauskas.mantas@gmail.com>
2025-05-28 00:58:45 +0300

    Delete UpDown

--- UpDown/src/Main.java
+++
@@ -1,9 +0,0 @@
-//mantas.lamauskas@mif.stud.vu.lt
-//šarūnas.gromavičius@mif.stud.vu.lt
-
-public class Main {
-// Main method
-    public static void main(String[] args) {
-        new GameWindow();
-    }
-}
