a7896898ffa2223f8573527288c41c7d060b2bd0
Šarūnas Gromavičius <sarasgrom@gmail.com>
2025-05-27 21:58:36 +0300

    ...

--- UpDown/src/Main.java
+++
@@ -1,9 +0,0 @@
-//mantas.lamauskas@mif.stud.vu.lt
-//šarūnas.gromavičius@mif.stud.vu.lt
-
-public class Main {
-// starts this mess of an application
-    public static void main(String[] args) {
-        new GameWindow();
-    }
-}
--- UpDown/src/SceneListener.java
+++
@@ -1,7 +0,0 @@
-//mantas.lamauskas@mif.stud.vu.lt
-//šarūnas.gromavičius@mif.stud.vu.lt
-
-public interface SceneListener {
-// Called when a scene is finally over, thank god.
-    void onSceneFinished();
-}
